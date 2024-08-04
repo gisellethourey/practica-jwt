aaf720c6b4b6f8915974a1a953791373fe45873a
Giselle Thourey <githourey@gmail.com>
2024-08-03 23:41:41 -0400

    tada: carga de archivos

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import userRoutes from "./routes/userRoutes.js";
+const app = express();
+const PORT = process.env.SERVER_PORT || 3000;
+
+//Middlewares
+//Routes
+app.use('/', userRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server on 🔥 http://localhost:${PORT}`);
+});
+
+
